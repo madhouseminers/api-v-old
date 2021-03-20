324fc815129e8ef299d5edc3930ec54cda3d62d6
Stevan Goode <steve@madhouseminers.com>
2021-03-20 12:59:43 +0000

    99Allow self-signed certs for SSL

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,7 @@
 import * as pg from "pg";
-const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL });
+const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+});
 
 export default pool;
